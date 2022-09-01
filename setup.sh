a2d7e7f49a52dfa1999fd91c8389044e2f6ce0ea
itsezc <chirub@foretag.co>
2022-09-01 17:50:41 +0100

    chore: add `setup.sh`

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# Alpine Linux
+
+apk update
+apk upgrade
+
+apk add --no-cache --update curl
+
+# Rust and Cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# TODO: NVM (Node & NPM) support for Alpine is pending (https://github.com/nvm-sh/nvm/issues/1102)
+
+# Bun
+curl https://bun.sh/install | bash
+
