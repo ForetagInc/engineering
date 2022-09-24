1ca175cf0ef3848c8db8b97b63334b52a133409b
itsezc <chirub@foretag.co>
2022-09-24 21:54:11 +0100

    add: `firefox-debug` to frontend-toolkit

--- packages/frontend-toolkit/CHANGELOG.md
+++ packages/frontend-toolkit/CHANGELOG.md
@@ -3,3 +3,5 @@
 All notable changes to the "foretag.frontend-toolkit" extension will be documented in this file.
 
 ## [0.1.7] - 2022-09-19
+### Added
+- Add Firefox Debugger to extension pack
---
+++ packages/vscode/extensions/file/size.ts
@@ -0,0 +1,6 @@
+import * as vscode from 'vscode';
+
+export function activateFileSize(context: vscode.ExtensionContext) {
+}
+
+export function deactivateFileSize(context: vscode.ExtensionContext) { }
